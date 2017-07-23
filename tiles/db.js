7dc50ae05da5667ec4da0b7c61db588e191b667f
seljydef <seljydef@gmail.com>
2017-07-24 00:37:11 +0900

    fix

--- tiles/db.js
+++ tiles/db.js
@@ -6,70 +6,65 @@
 // To run you must configure the postgis_settings variable
 
 
-var mapnik = require('mapnik');
-var mercator = require('./sphericalmercator');
-var url = require('url');
-var fs = require('fs');
-var http = require('http');
-var parseXYZ = require('./utils/tile.js').parseXYZ;
-var path = require('path');
-var port = 8000;
-var TMS_SCHEME = false;
+const mapnik = require('mapnik');
+const mercator = require('./sphericalmercator');
+const parseXYZ = require('./tile').parseXYZ;
+const path = require('path');
+
+const TMS_SCHEME = false;
 
 // register postgis plugin
 if (mapnik.register_default_input_plugins) mapnik.register_default_input_plugins();
 
 // change this to fit your db connection and settings
-var postgis_settings = {
-  'dbname' : 'test',
-  'table' : 'cultural_property',
-  'user' : 'postgres',
-  'type' : 'postgis',
-  'extent' : '-20005048.4188,-9039211.13765,19907487.2779,17096598.5401'  //change this if not merc
+const postgisSettings = {
+  dbname: 'test',
+  table: 'cultural_property',
+  user: 'postgres',
+  type: 'postgis',
+  extent: '-20005048.4188,-9039211.13765,19907487.2779,17096598.5401',
 };
 
-function createTiles(req, res) {
-  parseXYZ(req, TMS_SCHEME, function(err,params) {
+module.exports.createTiles = (req, res) => {
+  parseXYZ(req, TMS_SCHEME, (err, params) => {
     if (err) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
       res.end(err.message);
     } else {
       try {
-        var map = new mapnik.Map(256, 256, mercator.proj4);
-        var layer = new mapnik.Layer('tile', mercator.proj4);
-        var postgis = new mapnik.Datasource(postgis_settings);
-        var bbox = mercator.xyz_to_envelope(parseInt(params.x),
-                                               parseInt(params.y),
-                                               parseInt(params.z), false);
+        const map = new mapnik.Map(256, 256, mercator.proj4);
+        const vt = new mapnik.VectorTile(params.x, params.y, params.z);
+        const layer = new mapnik.Layer('tile', mercator.proj4);
+        const postgis = new mapnik.Datasource(postgisSettings);
+        const bbox = mercator.xyz_to_envelope(parseInt(params.x, 10),
+                                              parseInt(params.y, 10),
+                                              parseInt(params.z, 10), false);
 
         layer.datasource = postgis;
         layer.styles = ['point'];
 
         map.bufferSize = 64;
-        map.load(path.join(__dirname, 'point_vector.xml'), {strict: true}, function(err,map) {
-            if (err) throw err;
-            map.add_layer(layer);
+        map.load(path.join(__dirname, 'point_vector.xml'), { strict: true }, (err, map) => {
+          if (err) throw err;
+          map.add_layer(layer);
 
-            // console.log(map.toXML()); // Debug settings
-
-            map.extent = bbox;
-            var im = new mapnik.Image(map.width, map.height);
-            map.render(im, function(err, im) {
-              if (err) {
-                throw err;
-              } else {
-                res.writeHead(200, {'Content-Type': 'image/png'});
-                res.end(im.encodeSync('png'));
-              }
-            });
+          map.extent = bbox;
+          const im = new mapnik.Image(map.width, map.height);
+          map.render(im, (err, im) => {
+            if (err) {
+              throw err;
+            } else {
+              res.writeHead(200, { 'Content-Type': 'image/png' });
+              res.end(im.encodeSync('png'));
+            }
+          });
         });
       }
       catch (err) {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end(err.message);
       }
     }
   });
-}).listen(port);
+};
 
-console.log('Server running on port %d', port);

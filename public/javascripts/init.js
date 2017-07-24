e70713d5734e30b360af98b5a785192eeb8742c6
seljydef <seljydef@gmail.com>
2017-07-24 22:51:29 +0900

    add scene file

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,8 +1,8 @@
 function init(){
-	var map = L.map('map');
-  var layer = Tangram.leafletLayer({
-      scene: 'scene.yaml',
-      attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
+  const map = L.map('map');
+  const layer = Tangram.leafletLayer({
+    scene: 'scene.yaml',
+    attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>',
   });
   layer.addTo(map);
   map.setView([40.70531887544228, -74.00976419448853], 15);

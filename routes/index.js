cfbf1798df78319cf2fe86842ed6a279904640dc
seljydef <seljydef@gmail.com>
2017-07-25 21:42:45 +0900

    add tiles api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const tile = require('../tiles/tile');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/tiles', (req, res) => {
+  console.log('hoge');
+  tile.createQuery(req, (res) => {
+    res.status(200).set({ 'Content-Type': 'application/x-protobuf' });
+    res.end();
+  });
+});
+
 
 module.exports = router;

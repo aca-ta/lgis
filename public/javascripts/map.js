16ca1f5665e54df1c081409343f728e80e14f457
seljydef <seljydef@gmail.com>
2017-08-04 00:17:57 +0900

    fix as change the feature color when it clicked.

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,19 +1,26 @@
-function onFeatureClick(selection) {
-  if (!selection.feature) return;
-  alert(selection.feature.properties.name);
+function setOnFeatureClick(scene) {
+  const func = (selection) => {
+    if (!selection.feature) return;
+    scene.config.global.selected = selection.feature.properties.id;
+    alert(selection.feature.properties.name);
+    scene.rebuild();
+  };
+
+  return func;
 }
+
 
 function createMap() {
   const map = L.map('map');
   const layer = Tangram.leafletLayer({
     scene: 'scene.yaml',
-    events: {
-      click: onFeatureClick,
-    },
     attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>',
   });
   layer.addTo(map);
   layer.on('init', () => {
+    layer.setSelectionEvents({
+      click: setOnFeatureClick(layer.scene),
+    });
     addGUI(layer.scene);
   });
   layer.addTo(map);

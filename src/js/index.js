38a8a4405f7dfb15c8b3eab9994dc491cb596d5f
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-05-18 19:46:32 +0900

    delete dat.gui

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,14 @@
 import map from './map';
 import BaseLayer from './layer-base';
 import Layer from './layer';
-import GuiControler from './gui-control';
-import css from '../css/style.css';
 
 window.addEventListener('load', () => {
-  new BaseLayer(map, GuiControler);
+  new BaseLayer(map);
 }, false);
 
 
 /* add button events */
-document.getElementById('lgis-show-table').addEventListener('click', () => {
+document.querySelector('#lgis-show-table').addEventListener('click', () => {
   const settings = {
     host: document.getElementById('lgis-host').value,
     table: document.getElementById('lgis-table').value,

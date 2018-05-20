8f62c38ad63fc1e9e2e4cd87f171dc1f5d883481
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-05-20 18:09:54 +0900

    Use webpack for stylesheets.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-module.exports = {
-  entry: [
-    'babel-polyfill',
-    './src/js/index.js',
-  ],
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const javascripts = {
+  mode: 'development',
+  entry: ['babel-polyfill', './src/js/index.js'],
   output: {
     path: `${__dirname}/public/javascripts`,
     filename: 'lgis.js',
@@ -18,11 +18,39 @@ module.exports = {
           presets: ['env'],
         },
       },
-      {
-        test: /\.css$/,
-        loader: ['style-loader', 'css-loader']
-      },
     ],
   },
   devtool: 'source-map',
 };
+
+const css = {
+  mode: 'development',
+  entry: './src/css/style.scss',
+  output: {
+    path: `${__dirname}/public/stylesheets`,
+    filename: 'lgis.css',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract([
+          {
+            loader: 'css-loader',
+            options: {url: false},
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [`${__dirname}/node_modules/leaflet/dist`],
+            },
+          },
+        ]),
+      },
+    ],
+  },
+  plugins: [new ExtractTextPlugin('lgis.css')],
+};
+
+module.exports = [javascripts, css];

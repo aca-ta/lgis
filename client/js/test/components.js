d2362af4c230860b6169ced94d4e4d625ea69a00
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-07-25 00:19:37 +0900

    add test case for setting.

--- client/js/test/components.js
+++ client/js/test/components.js
@@ -1,15 +1,26 @@
-import React from "react";
-import { shallow } from "enzyme";
-import test from "tape-catch";
-import Map from "../map";
-import sinon from "sinon";
+import React from 'react';
+import {shallow} from 'enzyme';
+import test from 'tape-catch';
+import _ from 'lodash';
+import sinon from 'sinon';
+import Map from '../map';
+import setting from '../setting';
 
-test("Mounting the Map", assert => {
+test('Mounting the Map', assert => {
+  document.body.innerHTML = `<button id="lgis-show-table">button</button>`;
   const viewport = sinon.spy();
   const mapstyle = sinon.spy();
   const map = shallow(<Map viewport={viewport} mapstyle={mapstyle} />);
-  assert.ok(map.exists(), "Map mounted");
+  assert.ok(map.exists(), 'Map mounted');
   map.unmount();
-  assert.ok(true, "App unmounted");
+  assert.ok(true, 'App unmounted');
+  assert.end();
+});
+
+test('fetch JSON from setting', assert => {
+  document.body.innerHTML = `<textarea>{"test": "test"}</textarea>`;
+  const actual = setting('textarea');
+  const expected = {test: 'test'};
+  assert.ok(_.isEqual(actual, expected), 'setting test ok');
   assert.end();
 });
--- client/js/test/setup-browser-env.js
+++ client/js/test/setup-browser-env.js
@@ -4,6 +4,5 @@ import {configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 require('jsdom-global')();
-document.body.innerHTML = `<button id="lgis-show-table">button</button>`;
 
 configure({adapter: new Adapter()});

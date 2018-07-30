6b29731f2f0a8ea3084e19b5510a9fc4fa0c2c6d
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-07-30 09:22:14 +0900

    fix styles.

--- client/js/app.js
+++ client/js/app.js
@@ -26,7 +26,7 @@ const styles = theme => ({
     height: '100%',
     zIndex: 1,
     overflow: 'hidden',
-    position: 'relative',
+    position: 'absolute',
     display: 'flex',
     width: '100%',
   },
@@ -65,7 +65,6 @@ const styles = theme => ({
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,

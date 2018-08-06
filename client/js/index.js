c5e734b9d2cc692f3195a196193d7a94f4663402
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-08-07 08:53:59 +0900

    change id.

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
@@ -7,7 +7,7 @@ import App from './app';
 window.addEventListener(
   'load',
   () => {
-    ReactDOM.render(<App />, document.querySelector('#navbar'));
+    ReactDOM.render(<App />, document.querySelector('#app'));
   },
   false,
 );

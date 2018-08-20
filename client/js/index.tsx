6f07aa3125ec75e8280d953e2273865876b05386
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-08-20 09:00:57 +0900

    add redux.

--- client/js/index.tsx
+++ client/js/index.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { Lgis } from './lgis';
 
+const store = createStore(rootReducer);
 
 window.addEventListener(
   'load',

ebfc1bb7b5408f27c33ab76336d4e81b268f34b9
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-08-31 11:11:25 +0900

    fix.

--- client/js/models/layer.ts
+++ client/js/models/layer.ts
@@ -19,13 +19,13 @@ export const getLayer = (host: string, db: string, table: string) => {
   return {source, layer};
 };
 
-export const loadData = (mapStyle: any, settingJson: string, table: string) => {
+export const loadData = (prevMapStyle: any, settingJson: string, table: string) => {
   const settings = JSON.parse(settingJson);
   const {source, layer} = getLayer(settings.host, settings.db, table);
 
-  const prevMapStyle: any = mapStyle
+  const mapStyle: any = prevMapStyle
     .setIn(['sources', 'lgis'], fromJS(source))
-    .set('layers', mapStyle.get('layers').push(layer));
+    .set('layers', prevMapStyle.get('layers').push(layer));
 
   return mapStyle;
 };

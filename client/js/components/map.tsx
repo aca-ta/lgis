cf02aac005d89bb572807907e50c52addcc150e5
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-09-15 21:27:21 +0900

    show feature properties on a tooltip.

--- client/js/components/map.tsx
+++ client/js/components/map.tsx
@@ -1,4 +1,12 @@
 import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from '@material-ui/core';
 import ReactMapGL, {Viewport, Popup, MapEvent} from 'react-map-gl';
 import {connect} from 'react-redux';
 import {Dispatch} from 'redux';
@@ -17,12 +25,18 @@ const ToolTip = (
   isPopupOpen: boolean,
   latitude: number,
   longitude: number,
-  properties: {},
+  properties: {[key: string]: string},
   dispatchClosePopup: () => void,
 ) => {
   if (Object.keys(properties).length === 0) return;
-  const list: React.StatelessComponent[] = [];
-  _.forEach(properties, (value, key) => list.push(<div>value + key</div>));
+  const ParamList = Object.keys(properties).map(key => {
+    return (
+      <TableRow>
+        <TableCell>{key}</TableCell>
+        <TableCell>{properties[key]}</TableCell>
+      </TableRow>
+    );
+  });
 
   return (
     <Popup
@@ -30,7 +44,15 @@ const ToolTip = (
       longitude={longitude}
       anchor="top"
       onClose={() => dispatchClosePopup()}>
-      <div>{list}</div>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Key</TableCell>
+            <TableCell>Value</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>{ParamList}</TableBody>
+      </Table>
     </Popup>
   );
 };
@@ -69,7 +91,6 @@ const Map = (props: {
         dispatchChangeViewport(viewport)
       }
       onClick={(e: MapEvent, lngLat: number[], feature: Feature) =>
-        //TODO: featureの型を定義する
         dispatchOpenPopup(
           e.lngLat[0],
           e.lngLat[1],

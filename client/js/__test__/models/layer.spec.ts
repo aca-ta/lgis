c29031c25002ff544893aa503b033eee5359497a
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-10-10 21:59:13 +0900

    fix a test.

--- client/js/__test__/models/layer.spec.ts
+++ client/js/__test__/models/layer.spec.ts
@@ -1,16 +1,35 @@
+import {fromJS, List, Map } from 'immutable';
 import { addLayerStyle } from '../../models/layer';
+
 
 describe('addLayerStyle', () => {
   it('returns new mapStyle', () => {
 
-    const mapStyle = {
-      sources: {},
-      layers: {}
-    };
+    const mapStyle = fromJS({
+      sources: [],
+      layers: []
+    });
     const setting = "{}";
     const table = "test";
     const geomType = "point";
 
-    expect(addLayerStyle(mapStyle, setting, table, geomType)).toBe({});
+    const expected = fromJS({
+      "sources": [],
+      "layers":  [
+        {
+          "id": "point", 
+          "type": "circle",
+          "source": "lgis",
+          "source-layer":
+          "tile",
+          "interactive": true,
+          "paint": {
+            "circle-color": "#4153f4"
+          }
+        }
+      ]
+    });
+
+    expect(addLayerStyle(mapStyle, setting, table, geomType)).toEqual(expected);
   })
 });

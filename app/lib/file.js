fd8aef0ca61a6fd21fd5ddefff2f704ac0ca0ae6
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-10-18 09:27:10 +0900

    update: save as json.

--- app/lib/file.js
+++ app/lib/file.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 
-module.exports.write = (name) => {
+module.exports.write = (name, host, db, table, datum) => {
 
   const basepath = `etc/savedMaps/${name}.json`;
   try {
     const stream = fs.createWriteStream(basepath, 'utf8');
-    stream.write(name);
+
+    const saved = {
+      name,
+      host,
+      db,
+      table,
+      datum
+    }
+    stream.write(JSON.stringify(saved));
   
   } catch(err) {
     return err;
--- app/saveMap.js
+++ app/saveMap.js
@@ -2,7 +2,7 @@ const file = require('./lib/file');
 
 module.exports.save = (req, func) => {
   const msg = `${req.query.name} is saved`;
-  file.write(req.query.name)
+  file.write(req.query.name, req.query.host, req.query.db, req.query.table, req.query.datum)
   func(msg)
   return msg;
 }

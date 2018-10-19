1b3e204c1492b9cfa18c32101a2032da3f7bce66
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-10-19 09:06:30 +0900

    refactor.

--- app/saveMap.js
+++ app/saveMap.js
@@ -1,8 +1,14 @@
 const file = require('./lib/file');
 
 module.exports.save = (req, func) => {
-  const msg = `${req.query.name} is saved`;
-  file.write(req.query.name, req.query.host, req.query.db, req.query.table, req.query.datum)
-  func(msg)
+  file.write(
+    req.query.name,
+    req.query.host,
+    req.query.db,
+    req.query.table,
+    req.query.datum,
+  );
+  const msg = `save ${req.query.name}`;
+  func(msg);
   return msg;
-}
+};

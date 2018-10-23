8dc8091dac50b65c78add6737a5c35f5b2bdc27e
Shuhei Tsukaomoto <seljydef@gmail.com>
2018-10-23 09:29:00 +0900

    update: add history.

--- client/js/index.tsx
+++ client/js/index.tsx
@@ -1,15 +1,23 @@
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, Reducer} from 'redux';
+import {applyMiddleware, compose, createStore, Reducer} from 'redux';
 import {Lgis} from './components/lgis';
 import {reducer} from './reducers';
 
+const history = createBrowserHistory();
+
 const store = createStore(
-  reducer,
+  connectRouter(history)(reducer),
   typeof window !== 'undefined' &&
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
     (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+  compose(
+    applyMiddleware(
+    routerMiddleware(history),
+  ))
 );
 
 window.addEventListener(
